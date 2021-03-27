fed0c965bcf159955d2a387d5cf46ca43c6be11d
David Konsumer <konsumer@jetboystudio.com>
2021-03-27 14:36:55 -0700

    fancier command setup

--- bot.js
+++ bot.js
@@ -1,26 +1,34 @@
 const dotenv = require('dotenv')
 const Discord = require('discord.js')
+const { readdirSync } = require('fs')
 
 dotenv.config()
 const { DISCORD_TOKEN } = process.env
-
-const client = new Discord.Client()
 
 if (!DISCORD_TOKEN) {
   console.error('Please set DISCORD_TOKEN environment variable!')
   process.exit(1)
 }
 
+const client = new Discord.Client()
+
+// load all commands from commands directory and put into an object keyed by name
+const commands = readdirSync(`${__dirname}/commands`)
+  .map((r) => [r, require(`${__dirname}/commands/${r}`)])
+  .reduce((a, c) => {
+    return { ...a, [c[0].split('.')[0]]: c[1] }
+  }, {})
+
 client.once('ready', () => {
   console.log('Ready!')
 })
 
 client.on('message', message => {
-  console.log(message.content)
-  if (message.content === '!ping') {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send('Pong.')
-  }
+  Object.keys(commands).forEach(command => {
+    if (message.content.startsWith(`!${command}`)) {
+      commands[command](message, client)
+    }
+  })
 })
 
 client.login(DISCORD_TOKEN)
