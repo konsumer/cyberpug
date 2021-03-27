e54dbaf1d95fe6e31c8385337a4bbe25954d3f27
David Konsumer <konsumer@jetboystudio.com>
2021-03-27 14:13:44 -0700

    add ping/pong and log for testing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cyberpug
+
 A demo discord bot for our cooperative programming group.
+
+You can [invite it to your own server](https://discord.com/oauth2/authorize?client_id=825469215330271283&scope=bot)
+
+## local dev
+
--- bot.js
+++ bot.js
@@ -15,4 +15,12 @@ client.once('ready', () => {
   console.log('Ready!')
 })
 
+client.on('message', message => {
+  console.log(message.content)
+  if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.')
+  }
+})
+
 client.login(DISCORD_TOKEN)
