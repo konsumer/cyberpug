3647a029721a5d8dc09980989c4f4039855d7fa7
David Konsumer <konsumer@jetboystudio.com>
2021-03-27 14:49:51 -0700

    embed example

---
+++ commands/embed.js
@@ -0,0 +1,16 @@
+// an example embedded (fancy) message
+
+const { MessageEmbed } = require('discord.js')
+
+module.exports = (message, client) => {
+  const embed = new MessageEmbed()
+    // Set the title of the field
+    .setTitle('A slick little embed')
+    // Set the color of the embed
+    .setColor(0xff0000)
+    // Set the main content of the embed
+    .setDescription('Hello, this is a slick embed!')
+
+  // Send the embed to the same channel as the message
+  message.channel.send(embed)
+}
